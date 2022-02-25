7c79e8f62e4b6bc1fd0a59e96fa9cb5f99cc1827
Brianhz <62373576+Brianhz@users.noreply.github.com>
2022-02-25 08:49:52 +0800

    change on 20220225

--- website/main.py
+++ website/main.py
@@ -1,6 +1,16 @@
 from requests_html import HTMLSession
+from flask import Flask
 
 url = 'https://www.beerwulf.com/en-gb/c/beers?segment=Beers&catalogCode=Beer_1'
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Home Page"
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
 
 
 s = HTMLSession()
@@ -8,10 +18,28 @@ r = s.get(url)
 
 r.html.render(sleep=1)
 
+
 products = r.html.xpath('//*[@id="product-item-container"]', first=True)
 
 for item in products.absolute_links: 
     r = s.get(item)
-    print(r.html.find('div.product-detaili-info-title', first=True).text)
+    name = r.html.find('div.product-detaili-info-title', first=True).text
+    subtext =  r.html.find('div.product - subtext', first=True).text
+    price =  r.html.find('span.price', first=True).text
+
+    try:
+        rating =  r.html.find('div.product - subtext', first=True).text
+    except:
+        rating = 'none'
     
-    https://www.youtube.com/watch?v=gW-q6G9KYTg
+
+    if r.html.find('div.add-to-cartcontainer'):
+        stock = 'in stock'
+    else:
+        stock = 'out of stock'
+
+
+    print(name, subtext, rating, price, stock)
+    
+
+
