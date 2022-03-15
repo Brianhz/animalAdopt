8a1fd67c654d5cc58531c7f37e6974fa82130e86
Brianhz <62373576+Brianhz@users.noreply.github.com>
2022-03-15 19:54:59 +0800

    03150754

--- website/main.py
+++ website/main.py
@@ -1,17 +1,48 @@
 from requests_html import HTMLSession
-from flask import Flask
+from datetime import datetime
+from flask import Flask, render_template, url_for, flash, redirect, request, session
+from flask_admin import Admin 
+from flask_sqlalchemy import SQLAlchemy
+from forms import RegistrationForm, LoginForm, PostForm
+from sqlalchemy import Column, String, Integer
+from sqlalchemy import insert
+import os
 
 url = 'https://www.beerwulf.com/en-gb/c/beers?segment=Beers&catalogCode=Beer_1'
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'ggg'
+admin = Admin(app)
 
-@app.route("/")
+@app.route('/')
+
+@app.route("/home")
 def home():
-    return "Home Page"
+    #return "Home Page"#
+    return render_template('Home.html')
+
+@app.route("/about")
+def about():
+    return render_template('About.html')
+
+@app.route("/product")
+def product():
+    return render_template('Product.html')
+
+@app.route("/account")
+def account():
+    return render_template('Account.html')
+
+@app.route("/cart")
+def cart():
+    return render_template('Cart.html')
+
+@app.route("/wanted")
+def wanted():
+    return render_template('Wanted.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
-
+        app.run(debug=True, port=8000)
 
 s = HTMLSession()
 r = s.get(url)
@@ -39,7 +70,7 @@ for item in products.absolute_links:
         stock = 'out of stock'
 
 
-    print(name, subtext, rating, price, stock)
+    print(name, subtext, rating, price, stock) 
     
 
 
